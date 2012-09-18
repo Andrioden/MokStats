45f079a794d6648b6896a6cb5a8489c898c77489
Andre (home) <andre.ok@online.no>
2012-09-18 03:44:12 +0200

    Forcing appended slash for links at index

--- mokstats/static/script.js
+++ mokstats/static/script.js
@@ -1,3 +1,9 @@
 function sortBy(type) {
 	$('#players li').tsort("."+type, {order:'desc'})
 }
+
+function slashedRelativeHref(relativePath) {
+	var path = window.location.pathname;
+	if (path.charAt(path.length-1) != "/") path += "/";
+	window.location.href=path+relativePath;
+}
