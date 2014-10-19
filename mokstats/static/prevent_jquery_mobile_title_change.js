5a6528d4f7be84eac21a3147b117c8093fe8f86c
André Skoglund Hansen <andre.ok@online.no>
2014-10-19 19:53:09 +0200

    Changed a comment

--- mokstats/static/prevent_jquery_mobile_title_change.js
+++ mokstats/static/prevent_jquery_mobile_title_change.js
@@ -1,2 +1,3 @@
-// jquery mobile sets the visible page title, this line of code rewrite that to the correct
+// jquery mobile sets the visible page title, this line of code rewrite that to the correct title. 
+// Which is the one specified in <head>
 $(":jqmData(role='page')").attr("data-title", document.title);
