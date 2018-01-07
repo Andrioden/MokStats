767e0697e31501810e586119812f6f90622a569b
André S. Hansen <andre.ok@online.no>
2018-01-07 11:26:17 +0100

    Removed LANGUAGE_CODE from example settings

--- mokstats/settings_example.py
+++ mokstats/settings_example.py
@@ -79,10 +79,6 @@ MANAGERS = ADMINS
 # In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'Europe/Oslo'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-#LANGUAGE_CODE = 'no-bok'
-
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
